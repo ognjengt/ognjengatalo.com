b6a2444668091f580f64833805554f432a563458
Ognjen Gatalo <ognjeng95@gmail.com>
2018-01-03 12:10:14 +0100

    Commit what is done already

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Ognjen Gatalo</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="css/onepage-scroll.css" media="screen" title="no title">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="css/ionicons.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/component.css">
+
+  </head>
+  <body>
+
+    <div id="particles-js"></div>
+    <div class="main">
+      <section>
+        <div class="frameHolder">
+          <div class="frame" id="frame1">
+
+          <div class="container-fluid firstContainer">
+            <div class="col-md-6">
+              <div class="hero-text">
+                <h1><span class="typeName"></span></h1>
+                <h3><b><span class="typeTitle"></span></b></h3>
+                <p class="brief">
+                  Passionate, dedicated and innovative software programmer and web developer from Serbia, with the goal of building scalable and maintainable cross platform and web applications, along with responsive websites. Co-founder of IT solutions company <b><a href="http://www.thenamespace.com" class="scaleLink" target="_blank">Namespace</a></b>. Striving to bring innovative ideas to life, that is why I'm always giving 101% on every project. Open for freelance work, so feel free to contact me.
+                </p>
+              </div>
+            </div>
+
+            <div class="col-md-6 heroright">
+              <div class="hero-info" >
+                <ul>
+                  <a href="https://www.linkedin.com/in/ognjeng" target="_blank"><li class="hvr-sweep-to-right firstPop"><i class="ion-social-linkedin"></i><b></b></li></a>
+                  <a href="https://twitter.com/ognjengt" target="_blank"><li class="hvr-sweep-to-right secondPop"><i class="ion-social-twitter"></i> <b></b></li></a>
+                  <a href="https://github.com/ognjengt" target="_blank"><li class="hvr-sweep-to-right thirdPop"><i class="ion-social-github"></i> <b></b></li></a>
+                </ul>
+                <a href="#">
+                  <button class="hero-btn hvr-sweep-to-top">
+                    <b>Dive in</b>
+                  </button>
+                </a>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="menu-icon ion-navicon"></div>
+          <!--
+          <div class="arrowholder">
+            <h3><i class="ion-chevron-down"></i></h3>
+          </div>--> <!--TODO: mozda ubaciti i ovu strelicu dole-->
+
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class="frameHolder">
+          <div class="frame" id="frame2">
+
+            <nav class="cl-effect-2">
+              <a href="#" data-hover="Dive in"><span>Dive in</span></a>
+            </nav>
+
+            <div class="parallaxdotholder layer" data-depth="0.25">
+              <div class="dot1 hvr-sweep-to-top " >
+                <div class="text"><b>ABOUT</b></div>
+              </div>
+            </div>
+
+            <div class="parallaxdotholder layer" data-depth="0.20">
+              <div class="dot2 hvr-sweep-to-top">
+                <div class="text"><b>SKILLS</b></div>
+              </div>
+            </div>
+
+            <div class="parallaxdotholder layer" data-depth="0.30">
+              <div class="dot3 hvr-sweep-to-top">
+                <div class="text"><b>ABOUT</b></div>
+              </div>
+            </div>
+
+            <div class="parallaxdotholder layer" data-depth="0.50">
+              <div class="dot4 hvr-sweep-to-top">
+                <div class="text"><b>ABOUT</b></div>
+              </div>
+            </div>
+
+
+
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class="frameHolder">
+          <div class="frame">
+            <div class="flexbox">
+            <div class="container-fluid projectcontainer">
+              <div class="col-md-3">
+                <div class="picturebox">
+                  <img src="img/namespace.jpg" alt="">
+                </div>
+              </div>
+                <div class="col-md-3">
+                  <div class="picturebox">
+                    <img src="img/devlib.png" alt="">
+                  </div>
+                </div>
+                <div class="col-md-3">
+                  <div class="picturebox">
+                    <img src="img/devlib.png" alt="">
+                  </div>
+                </div>
+                <div class="col-md-3">
+                  <div class="picturebox">
+                    <img src="img/devlib.png" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/particles.min.js"></script>
+    <script src="js/jquery.onepage-scroll.min.js"></script>
+    <script src="js/app.js"></script>
+    <script src="js/typed.min.js"></script>
+    <script src="js/interaction.js"></script>
+    <script src="js/parallax.min.js"></script>
+    <script src="js/modernizr.custom.js"></script>
+  </body>
+</html>
---
+++ js/interaction.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+  $(".main").onepage_scroll({
+    animationTime: 800
+  });
+
+  writeName();
+
+  $('.hero-btn').on('click', function() {
+    $('.main').moveDown();
+  })
+
+  function writeName() {
+    setTimeout(function(){
+      $(function(){
+      $(".typeName").typed({
+        strings: ["Ognjen ^100 Gatalo"],
+        typeSpeed: 60,
+        showCursor: true,
+        cursorChar: "|",
+        callback: writeTitle
+        });
+      });
+    },500)
+  }
+
+  function writeTitle() {
+    setTimeout(function(){
+      $(function(){
+      $(".typeTitle").typed({
+        strings: ["Software Engineer / Web Developer"],
+        typeSpeed: 12,
+        showCursor: true,
+        cursorChar: "|",
+        callback: removeCursor
+        });
+      });
+    },100)
+  }
+
+  function removeCursor() {
+    setTimeout(function() {
+      $('.typed-cursor').css({
+        "animation":"none"
+      }).fadeOut();
+  
+    },100);
+    $('.brief').fadeIn();
+  }
+  //parallax
+  var scene = document.getElementById('frame2');
+  var parallax = new Parallax(scene);
+
+})
